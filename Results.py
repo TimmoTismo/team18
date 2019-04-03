846ad1f222ead3a27d4265e3f2e27fd41160aabe
Jake Casey <jakecasey98@outlook.com>
2019-04-03 20:50:51 +0100

    Update Results.py

--- Results.py
+++ Results.py
@@ -13,6 +13,7 @@ class Results(Frame): #creating the frame for the results to be shown
 		def newPage(self, student_score):
 			Label(self, width="10", height="2").grid(row=0, column=0)
 			Label(self, text="Student Results", font=('Helvetica', 8)).grid(row=2, column=0)
+			#Results from the tests are stored with the username and scores in a csv file, this would have been read into here and then used to display the results.
 
 			#feedback and results would have been imported from relative csv, then using the currentuser would show only the relevant data here in future version
 root = Tk()
