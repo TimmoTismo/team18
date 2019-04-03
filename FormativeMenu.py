6906eb3b08b5a8d261bc6503e4fd97f7f26bae6a
Jake Casey <jakecasey98@outlook.com>
2019-04-03 20:53:09 +0100

    Update FormativeMenu.py

--- FormativeMenu.py
+++ FormativeMenu.py
@@ -4,3 +4,5 @@ import tkinter.messagebox
 class LoginFrame(Frame):
     def __init__(self, master): #tkinter initialisation
         super().__init__(master)
+        #would have for loop for each csv file exisiting or been created by the lecturer, create a button for each.
+        #each button would load either the Formative.py or Summative.py , then set the file within this to the relevant .csv
